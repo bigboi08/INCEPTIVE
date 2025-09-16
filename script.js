d8cf7eb08404f8458860d9e91a1b473c35c7d64a
bigboi08 <aminharsh08@gmail.com>
2025-09-16 00:10:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
+// Local background images
 const images = [
-  "https://images.unsplash.com/photo-1601560896164-834650b0b2ed?ixlib=rb-4.0.3&auto=format&fit=crop&w=1600&q=80", // fighter jet
-  "https://images.unsplash.com/photo-1582745071180-fbaf240d4f59?ixlib=rb-4.0.3&auto=format&fit=crop&w=1600&q=80", // carrier
-  "https://images.unsplash.com/photo-1507537297725-24a1c029d3ca?ixlib=rb-4.0.3&auto=format&fit=crop&w=1600&q=80", // Marines
-  "https://images.unsplash.com/photo-1504718855392-c0f33b1b2a00?ixlib=rb-4.0.3&auto=format&fit=crop&w=1600&q=80"  // flag
+  "images/jets.jpg",
+  "images/carrier.jpg",
+  "images/marines.jpg",
+  "images/flag.jpg"
 ];
 
 let current = 0;
 const heroBg = document.querySelector('.hero-bg');
 
-// preload images
+// Preload images
 images.forEach(src => {
   const img = new Image();
   img.src = src;
@@ -25,7 +26,6 @@ function changeBackground() {
   }
 }
 
-// initial + rotate
+// Initial + rotate
 changeBackground();
 setInterval(changeBackground, 6000);
-
